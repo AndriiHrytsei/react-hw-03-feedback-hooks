20f6cd0b059a3948cab5defedc1836a44f89c2d2
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2023-07-17 15:38:39 +0200

    format document

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
-export default function Notification({ message }){
-    return(
-        <>
-            <p>{message}</p>
-        </>
-    )
+export default function Notification({ message }) {
+  return (
+    <>
+      <p>{message}</p>
+    </>
+  );
 }
 
 Notification.propType = {
-    message: PropTypes.string.isRequired
-}
+  message: PropTypes.string.isRequired,
+};
